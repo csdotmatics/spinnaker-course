196edc30f245e218012392fbd7e3b99d86dd4a12
Csaba Soti <csaba.soti@dotmatics.com>
2019-10-24 07:09:44 +0100

    auto accept halyard question

--- scripts/3-install-halyard.sh
+++ scripts/3-install-halyard.sh
@@ -3,7 +3,7 @@
 set -e
 
 curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
-sudo bash InstallHalyard.sh --user ubuntu
+sudo bash InstallHalyard.sh --user ubuntu -y
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 sudo usermod -aG docker ubuntu
